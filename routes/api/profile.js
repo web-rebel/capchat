a970661dfbdb740f4af733e3ef2029aaa42384eb
web-rebel <w3bre8el@gmail.com>
2019-08-30 22:39:22 +0100

    Add Update Delete User Profile Experience

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -154,5 +154,119 @@ router.delete('/', auth, async (req, res) => {
     }
 });
 
+/**
+ * @route   PUT api/profile/experience
+ * @desc    Add profile experience
+ * @access  Private
+ */
+router.put('/experience', [ auth, [
+    check('title', 'Title is required').not().isEmpty(),
+    check('company', 'Company is required').not().isEmpty(),
+    check('from', 'From date is required').not().isEmpty(),
 
+]], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()){
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { title, company, location, from, to, current, description } = req.body;
+
+    const newExperience = {
+        title,
+        company,
+        location,
+        from,
+        to,
+        current,
+        description
+    }
+
+    try {
+        const profile = await Profile.findOne({ user: req.user.id });
+
+        if (!profile) { 
+            return res.status(400).json({ msg: 'No profile was found'});
+        }
+
+        profile.experience.unshift(newExperience);
+
+        await profile.save();
+
+        res.json(profile);
+
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+/**
+ * @route   PUT api/profile/experience/:exp_id
+ * @desc    Update profile experience
+ * @access  Private
+ */
+router.put('/experience/:exp_id', auth, async (req, res) => {
+    
+    const { title, company, location, from, to, current, description } = req.body;
+
+    const newExperience = {
+        title,
+        company,
+        location,
+        from,
+        to,
+        current,
+        description
+    }
+    
+    try {
+
+        const profile = await Profile.findOneAndUpdate({ user: req.user.id });
+
+        if (!profile){
+            return res.status(400).json({ msg: 'No profile was found' });
+        } 
+
+        const updateIndex = profile.experience.map(item => item.id).indexOf(req.params.exp_id);
+
+        profile.experience.splice(updateIndex, 1, newExperience);
+
+        await profile.save();
+
+        res.json(profile);
+
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+/**
+ * @route   DELETE api/profile/experience/:exp_id
+ * @desc    Delete profile experience
+ * @access  Private
+ */
+router.delete('/experience/:exp_id', auth, async (req, res) => {
+    try {
+
+        const profile = await Profile.findOne({ user: req.user.id });
+
+        if (!profile){
+            return res.status(400).json({ msg: 'No profile was found' });
+        } 
+
+        const removeIndex = profile.experience.map(item => item.id).indexOf(req.params.exp_id);
+
+        profile.experience.splice(removeIndex, 1);
+
+        await profile.save();
+
+        res.json(profile);
+
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server Error');
+    }
+});
 module.exports = router;
