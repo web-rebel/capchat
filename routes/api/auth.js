8da7646607fcd2ef377bd40b8a523b3236e14d7a
web-rebel <w3bre8el@gmail.com>
2019-08-30 13:46:46 +0100

    Login API

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { check, validationResult } = require('express-validator');
 
 const User = require('../../models/User');
 
@@ -16,6 +20,59 @@ router.get('/', auth, async (req, res) => {
         res.json(user);
     } catch (error) {
         console.error(error.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+/**
+ * @route   POST api/auth
+ * @desc    Auth user
+ * @access  Public
+ */
+router.post('/', [
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Password is Required').exists()
+], async (req, res) => {
+
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    try {
+        let user = await User.findOne({ email });
+
+        if(!user) {
+            return res.status(400).json({ errors: [{ msg: 'Invalid Credentials'} ]});
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if(!isMatch){
+            return res.status(400).json({ errors: [{ msg: 'Invalid Credentials'} ]});
+        }
+        
+        const payload = {
+            user: {
+                id: user.id
+            }
+        }
+
+        jwt.sign(
+            payload, 
+            config.get('jwtSecret'), { 
+                expiresIn: 3600000
+            }, 
+            (error, token) => {
+                if(error) throw error;
+                res.json({ token });
+            }   
+        );
+
+    } catch (error) {
+        console.log(error.message);
         res.status(500).send('Server Error');
     }
 });
