9609825d91ac89e878a84fcc76ecc3b75306ca30
web-rebel <w3bre8el@gmail.com>
2019-08-31 11:48:55 +0100

    Read Delete Posts

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -43,4 +43,73 @@ router.post('/', [ auth, [
     }
 });
 
+/**
+ * @route   GET api/posts
+ * @desc    Get all posts
+ * @access  Private
+ */
+router.get('/', auth, async (req, res) => {
+    try {
+        const posts = await Post.find().sort({ date: -1 });
+        res.json(posts);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server Error')
+    }
+});
+
+/**
+ * @route   GET api/posts/:id
+ * @desc    Get post by id
+ * @access  Private
+ */
+router.get('/:id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+
+        if (!post){
+            return res.status(404).json({ msg: 'Post Not Found' });
+        }
+
+        res.json(post);
+    } catch (error) {
+        console.error(error.message);
+        if (error.kind === 'ObjectId'){
+            return res.status(404).json({ msg: 'Post Not Found' });
+        }
+        res.status(500).send('Server Error')
+    }
+});
+
+/**
+ * @route   DELETE api/posts/:id
+ * @desc    Delete a posts by id
+ * @access  Private
+ */
+router.delete('/:id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+
+        if (!post){
+            return res.status(404).json({ msg: 'Post Not Found' });
+        }
+
+        // Check if user
+        if (post.user.toString() !== req.user.id) {
+            return res.status(401).json({ msg: 'User not Authorized' });
+        }
+
+        await post.remove();
+
+        res.json({ msg: 'Post Removed' });
+
+    } catch (error) {
+        console.error(error.message);
+        if (error.kind === 'ObjectId'){
+            return res.status(404).json({ msg: 'Post Not Found' });
+        }
+        res.status(500).send('Server Error')
+    }
+});
+
 module.exports = router;
